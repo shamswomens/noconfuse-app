f0df6730c30d13ac3cd7a1674273a8d8e5cf6db8
shamswomens <shamswomensworld@gmail.com>
2026-07-12 08:25:43 +0530

    Update theme.dart

--- noconfuse_app/lib/theme.dart
+++ noconfuse_app/lib/theme.dart
@@ -34,7 +34,7 @@ ThemeData buildAppTheme() {
       bodyColor: AppColors.textPrimary,
       displayColor: AppColors.textPrimary,
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       color: AppColors.surface,
       elevation: 0,
       shape: RoundedRectangleBorder(
